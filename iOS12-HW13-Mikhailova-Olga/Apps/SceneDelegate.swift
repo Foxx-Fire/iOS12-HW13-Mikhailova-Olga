566447e9d03e3b6c21fe831c35af25fbb887cb83
Olga Mikhailova <foxx_fire@mail.ru>
2024-01-27 23:00:55 +0300

    Structure

---
+++ iOS12-HW13-Mikhailova-Olga/Apps/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  iOS12-HW13-Mikhailova-Olga
+//
+//  Created by mbpro2.0/16/512 on 27.01.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    
+}
+
